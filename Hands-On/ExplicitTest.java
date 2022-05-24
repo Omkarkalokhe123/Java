ca4b19e2854082cb96aaf0d49aad466d08436bff
Omkarkalokhe123 <105192755+Omkarkalokhe123@users.noreply.github.com>
2022-05-24 12:34:09 +0530

    Add files via upload

---
+++ Hands-On/ExplicitTest.java
@@ -0,0 +1,13 @@
+package corejava;
+//Java program to illustrate type casting
+public class ExplicitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+byte b=-50;
+
+int i=(int)(b*2);
+System.out.println(i);
+}
+
+}
