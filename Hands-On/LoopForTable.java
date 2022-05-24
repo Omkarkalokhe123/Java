462489bf32dd1ad9da7de4a3f6a026563524dc0e
Omkarkalokhe123 <105192755+Omkarkalokhe123@users.noreply.github.com>
2022-05-24 12:23:33 +0530

    Add files via upload

---
+++ Hands-On/LoopForTable.java
@@ -0,0 +1,18 @@
+package corejava;
+
+public class LoopForTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Create scanner class object
+		
+		int n=128;
+		int i,k;
+
+		for (i=1;i<=10;i++){
+			 k = n * i;
+			System.out.println(n+ "*"+ i + " = " + k);
+		}
+	}
+	
+}
