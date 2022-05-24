9f80f5b9e263a4a36913e1c5682e8c2ecc81f2e7
Omkarkalokhe123 <105192755+Omkarkalokhe123@users.noreply.github.com>
2022-05-24 12:35:41 +0530

    Add files via upload

---
+++ Hands-On/arithmeticoperation.java
@@ -0,0 +1,32 @@
+package corejava;
+
+public class arithmeticoperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num1,num2,add,sub,div,mul,mod;
+
+num1=5;
+num2=6;
+
+    //for addition two nos.
+add=num1+num2;
+   //for subtraction two nos.
+sub=num1-num2;
+   //for division two nos.
+div=num1/num2;
+  // for multiplication two nos.
+mul=num1*num2;
+ // for modulo two nos.
+mod=num1%num2;
+
+// Print result to console.
+System.out.println("addtion is: "+add);
+System.out.println("subtraction is: "+sub);
+System.out.println("multiplication is: "+mul);
+System.out.println("division: "+div);
+System.out.println("Modulo : "+mod);
+
+	}
+
+}
