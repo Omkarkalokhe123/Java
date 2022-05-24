b22870ec88a3e95783bdcb8b40c17b4137a424a8
Omkarkalokhe123 <105192755+Omkarkalokhe123@users.noreply.github.com>
2022-05-24 12:25:23 +0530

    Add files via upload

---
+++ Hands-On/ImplicitTesting.java
@@ -0,0 +1,18 @@
+package corejava;
+
+public class ImplicitTesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 50;
+		 
+		//automatic type conversion
+		double d = i;
+		 
+		
+		System.out.println("Int value "+i);
+		System.out.println("Double value "+d);
+		
+	}
+
+}
